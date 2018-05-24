d18b542d6b8f5738f8bac4ede45ced0addeb54e2
moute <maximemoute@gmail.com>
2018-05-24 15:21:29 +0200

    Annotations adresse client entreprise

--- Xcoiffure_model/src/main/java/model/Client.java
+++ Xcoiffure_model/src/main/java/model/Client.java
@@ -1,22 +1,63 @@
 package model;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+@Entity
+@Table(name="client")
 public class Client {
 
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="CLI_ID")
 	private int id;
+	
+	@Column(name="CLI_NOM", nullable=false)
+	@NotEmpty
+	@Size(max=50)
 	private String nom;
+	
+	@Column(name="CLI_PRENOM", nullable=false)
+	@NotEmpty
+	@Size(max=50)
 	private String prenom;
-	private Adresse adresse = new Adresse();
+	
+	@Column(name="CLI_DATE_NAISSANCE", nullable=true)
+	@Temporal(TemporalType.DATE)
+	@NotNull
 	private String naissance;
+	
+	@ManyToMany  (cascade= {CascadeType.REMOVE , CascadeType.REFRESH })
+	@JoinTable(
+	name="adrclient",
+	joinColumns=@JoinColumn(name="ADRCLIENT_CLI_ID", referencedColumnName="CLI_ID"),
+	inverseJoinColumns=@JoinColumn(name="ADRCLIENT_ADR_ID", referencedColumnName="ADR_ID"))
+	private List<Adresse> clientAssocies;
 	
 	
 	public String toString() {
 	return this.prenom + " " + this.nom;
 	}
-	
+
 	
 	public int getId() {
 		return id;
@@ -24,6 +65,7 @@ public class Client {
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	
 	public String getNom() {
 		return nom;
@@ -31,6 +73,7 @@ public class Client {
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+
 	
 	public String getPrenom() {
 		return prenom;
@@ -38,13 +81,7 @@ public class Client {
 	public void setPrenom(String prenom) {
 		this.prenom = prenom;
 	}
-	
-	public Adresse getAdresse() {
-		return adresse;
-	}
-	public void setAdresse(Adresse adresse) {
-		this.adresse = adresse;
-	}
+
 	
 	public String getNaissance() {
 		return naissance;
@@ -52,7 +89,15 @@ public class Client {
 	public void setNaissance(String naissance) {
 		this.naissance = naissance;
 	}
+
 	
+	public List<Adresse> getClientAdresses() {
+		return clientAssocies;
+	}
+	public void setClientAdresses(List<Adresse> clientAdresses) {
+		this.clientAssocies = clientAdresses;
+	}
+
 	
 	public Client() {
 }
--- Xcoiffure_model/src/main/java/model/Entreprise.java
+++ Xcoiffure_model/src/main/java/model/Entreprise.java
@@ -1,34 +1,88 @@
 package model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+
+@Entity
+@Table(name="entreprise")
 public class Entreprise {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="ENT_ID")
 	private int id;
-	private String nom;
+	
+	@Column(name="ENT_NOM_SOCIETE", nullable=false)
+	@NotEmpty
+	@Size(max=50)
+	private String nom_societe;
+	
+	@Column(name="ENT_NOM_GERANT", nullable=false)
+	@NotEmpty
+	@Size(max=50)
 	private String nom_gerant;
 	
+	@OneToMany(mappedBy="Entreprise")
+	private List<Service> services;
 	
+	@ManyToMany  (cascade= {CascadeType.REMOVE , CascadeType.REFRESH })
+	@JoinTable(
+	name="adrfournisseur",
+	joinColumns=@JoinColumn(name="ADRFOURNISSEUR_FOU_ID", referencedColumnName="FOU_ID"),
+	inverseJoinColumns=@JoinColumn(name="ADRFOURNISSEUR_ADR_ID", referencedColumnName="ADR_ID"))
+	private List<Adresse> fournisseurAssocies;
+	
+
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
-	
-	public String getNom() {
-		return nom;
+
+	public String getNom_societe() {
+		return nom_societe;
 	}
-	public void setNom(String nom) {
-		this.nom = nom;
+	public void setNom_societe(String nom_societe) {
+		this.nom_societe = nom_societe;
 	}
-	
+
 	public String getNom_gerant() {
 		return nom_gerant;
 	}
 	public void setNom_gerant(String nom_gerant) {
 		this.nom_gerant = nom_gerant;
 	}
-	
-	
+
+	public List<Service> getServices() {
+		return services;
+	}
+	public void setServices(List<Service> services) {
+		this.services = services;
+	}
+
+	public List<Adresse> getFournisseurAdresses() {
+		return fournisseurAssocies;
+	}
+	public void setFournisseurAdresses(List<Adresse> fournisseurAdresses) {
+		this.fournisseurAssocies = fournisseurAdresses;
+	}
+
 	public Entreprise() {
 }
 
