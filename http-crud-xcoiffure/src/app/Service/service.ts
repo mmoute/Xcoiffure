23f7a465769754bf96793a1fd2652b7935691c51
Unknown <marine_duplessix@hotmail.com>
2018-06-25 16:01:58 +0200

    service crud-html

--- http-crud-xcoiffure/src/app/Service/service.ts
+++ http-crud-xcoiffure/src/app/Service/service.ts
@@ -1,5 +1,7 @@
+import {Entreprise} from '../Entreprise/entreprise';
+
 export class Service {
   public id: number = 0;
-  constructor(public nomService?: string, public prix?: number, public temps?: number, public entreprise?: Entreprise, public rdvs?: List<Rdv>) {
+  constructor(public nomService?: string, public prix?: number, public temps?: number, public entreprise?: Entreprise) {
   }
 }
