390c3b712f4a4a21b329bff1b19368b604af8a76
Unknown <marine_duplessix@hotmail.com>
2018-06-25 15:45:12 +0200

    http: crud /service

--- http-crud-xcoiffure/src/app/Service/api-service.service.ts
+++ http-crud-xcoiffure/src/app/Service/api-service.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import {AppConfigService} from '../app-config.service';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Service} from './service';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class ApiServiceService {
 
-  constructor() { }
+  constructor(private appConfig: AppConfigService, private http: HttpClient) {
+  }
+
+  public findAll(): Observable<Service[]> {
+    return this.http.get<Service[]>(`${this.appConfig.url}/service`);
+  }
+
+  public create(service: Service): Observable<void> {
+    return this.http.post<void>(`${this.appConfig.url}/service`, service);
+  }
+
+  public findById(id: number): Observable<Service> {
+    return this.http.get<Service>(`${this.appConfig.url}/service/` + id);
+  }
+
+  public update(service: Service): Observable<void> {
+    return this.http.put<void>(`${this.appConfig.url}/service/` + service.id, service);
+  }
+
+  public deleteById(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.appConfig.url}/service/` + id);
+  }
 }
--- http-crud-xcoiffure/src/app/Service/service.component.ts
+++ http-crud-xcoiffure/src/app/Service/service.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Service} from './service';
+import {ApiServiceService} from './api-service.service';
 
 @Component({
   selector: 'app-service',
@@ -6,10 +8,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./service.component.css']
 })
 export class ServiceComponent implements OnInit {
+  public services: Array<Service> = new Array<Service>();
+  public filtre: string = '';
+  public formService: Service = null;
 
-  constructor() { }
+  constructor(public servicesService: ApiServiceService) {
+    this.list();
+  }
 
   ngOnInit() {
   }
+  public sauvegarder() {
+    if (this.formService.id) {
+      this.servicesService.update(this.formService).subscribe(data => this.list());
+    } else {
+      this.servicesService.create(this.formService).subscribe(data => this.list());
+    }
+    this.formService = null;
+  }
 
+  public filtrer() {
+    return this.services.filter(c =>
+      c.nomService.toLowerCase().indexOf(this.filtre.toLowerCase()) !== -1);
+  }
+
+  public ajouter() {
+    this.formService = new Service();
+  }
+
+  public editer(id: number) {
+    this.servicesService.findById(id).subscribe(resp => {
+      this.formService = resp;
+    });
+  }
+
+  public supprimer(id: number) {
+    this.servicesService.deleteById(id).subscribe(data => this.list());
+  }
+
+  private list() {
+
+    this.servicesService.findAll().subscribe(resp => {
+      this.services = resp;
+    });
+  }
 }
